7276f909976d41458d64d1ac983386f22d45eef2
DataCruncher2 <134352299+DataCruncher2@users.noreply.github.com>
2023-05-25 13:50:50 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,14 +41,11 @@ try:
       streamlit.dataframe(back_from_function)
 except URLError as e:
    streamlit.error()
-
-#don't run anything past here while we troubleshoot
-streamlit.stop()
-
+   
 #import snowflake.connector
 streamlit.header("The fruit load list contains:")
-#Snowflake-related functions
 
+#Snowflake-related functions
 def get_fruit_load_list():
    with my_cnx.cursor() as my_cur:
       my_cur.execute("SELECT * from fruit_load_list")
@@ -60,6 +57,9 @@ if streamlit.button('Get fruit load list'):
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 
+#don't run anything past here while we troubleshoot
+streamlit.stop()   
+   
 #Allow the end user to add a fruit to the list
 streamlit.header("What fruit would you like to add")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
